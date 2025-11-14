d817589adccf4c851cdb2c86ec149ee1fed1ca83
AnkitMauryaEleven <154679649+AnkitEleven@users.noreply.github.com>
2025-11-14 15:55:44 +0530

    Update API endpoint for fetching users

--- frontend/src/components/Admin/AdminPatient.jsx
+++ frontend/src/components/Admin/AdminPatient.jsx
@@ -12,7 +12,7 @@ function AdminPatient() {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "https://hmsmern.onrender.com/admin/get-users"
+          "https://healthcare-x8vk.onrender.com/admin/get-users"
         );
         setUsers(response.data);
       } catch (error) {
