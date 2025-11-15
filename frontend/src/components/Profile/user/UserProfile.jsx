ec0e9b137f2138ab82c6ae3a3f22708bcea60477
AnkitMauryaEleven <154679649+AnkitEleven@users.noreply.github.com>
2025-11-15 16:09:37 +0530

    Update UserProfile.jsx

--- frontend/src/components/Profile/user/UserProfile.jsx
+++ frontend/src/components/Profile/user/UserProfile.jsx
@@ -84,104 +84,10 @@ function UserProfile() {
       <div className="h-[80%] w-[80%] bg-white shadow-xl p-2 flex">
         <UserSidebar profiePic={profiePic} userName={userData.userName} />
         <div className=" w-[70%] ms-24 p-4 flex flex-col justify-around ">
-          <p className="font-semibold text-3xl">Account Settings</p>
-          <form action="" className="flex flex-col h-[80%] justify-between">
-            <div className="w-full flex justify-between">
-              <div className="flex flex-col w-[50%] justify-start">
-                <p>Enter Your Name:</p>
-                <input
-                  value={userName}
-                  onChange={(e) => setName(e.target.value)}
-                  className="flex h-10 w-[90%] rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                  type="text"
-                  placeholder="Name"
-                ></input>
-              </div>
-              <div className="flex flex-col w-[50%] justify-start">
-                <p>Enter Your Email:</p>
-                <input
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="flex h-10  w-[90%] rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                  type="email"
-                  placeholder="Email"
-                ></input>
-              </div>
-            </div>
-            <div className="w-full flex justify-between">
-              <div className="flex flex-col w-[50%] justify-start">
-                <p>Enter Your Phone:</p>
-                <input
-                  value={mobileNumber}
-                  onChange={(e) => setMobileNumber(e.target.value)}
-                  className="flex h-10 w-[90%] rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                  type="text"
-                  placeholder="Phone"
-                ></input>
-              </div>
-              <div className="flex flex-col w-[50%] justify-start">
-                <p>Enter Your DOB:</p>
-                <input
-                  value={dateOfBirth}
-                  onChange={(e) => setdateofBirth(e.target.value)}
-                  className="flex h-10  w-[90%] rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                  type="date"
-                  placeholder="Name"
-                ></input>
-              </div>
-            </div>
-
-            <div className="w-full flex justify-between">
-              <div className="flex flex-col w-[50%] justify-start">
-                <p>Enter Your Gender:</p>
-                <input
-                  value={gender}
-                  onChange={(e) => setGender(e.target.value)}
-                  className="flex h-10 w-[90%] rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                  type="text"
-                  placeholder="Male/Female/Others"
-                ></input>
-              </div>
-              <div className="flex flex-col w-[50%] justify-start">
-                <p>Enter Your City:</p>
-                <input
-                  value={city}
-                  onChange={(e) => setCity(e.target.value)}
-                  className="flex h-10  w-[90%] rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                  type="text"
-                  placeholder="City"
-                ></input>
-              </div>
-            </div>
-            <div className="w-full flex justify-between">
-              <div className="flex flex-col w-[50%] justify-start">
-                <p>Enter Your State:</p>
-                <input
-                  value={state}
-                  onChange={(e) => setState(e.target.value)}
-                  className="flex h-10 w-[90%] rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                  type="text"
-                  placeholder="State"
-                ></input>
-              </div>
-              <div className="flex flex-col w-[50%] justify-start">
-                <p>Enter Your Address:</p>
-                <input
-                  value={address}
-                  onChange={(e) => setAddress(e.target.value)}
-                  className="flex h-10  w-[90%] rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                  type="text"
-                  placeholder="Address"
-                ></input>
-              </div>
-            </div>
-            <button
-              onClick={handleUpdate}
-              className="bg-black w-[95%] text-white p-2 rounded-full"
-            >
-              Update
-            </button>
-          </form>
+          <p className="text-green-700 font-semibold text-2xl">
+        Click on Appointment to Book Appointment
+      </p>
+            
         </div>
       </div>
     </section>
